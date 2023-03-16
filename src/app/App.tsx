ee7c2f3963dc70093f3784c1633778c461edf445
Nikolai Geldyev <nikoejento@gmail.com>
2023-03-16 15:35:35 +0400

    fix: replace router to hash-router to prevent errors on gh-pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import { AuthForm } from "src/features";
 
 import styles from "./App.module.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <AuthForm />,
